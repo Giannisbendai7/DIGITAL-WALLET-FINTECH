d03f1f25eef0c66e77c9f3a7fd6e406a8a64051e
Giannis Bendai (John Bendaj) <165202524+Giannisbendai7@users.noreply.github.com>
2026-04-27 00:15:42 +0300

    Update train.py

--- train.py
+++ train.py
@@ -1,7 +1,9 @@
-from sklearn.linear_model import LinearRegression
 import joblib
+from sklearn.linear_model import LinearRegression
 
 def train(X, y):
     model = LinearRegression()
     model.fit(X, y)
+
     joblib.dump(model, "model.pkl")
+    return model
